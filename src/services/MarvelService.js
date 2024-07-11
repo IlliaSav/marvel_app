b084b75df4db2800b061d2b87819f5963803cfd0
Illia <examle@gmail.com>
2024-07-11 15:52:36 +0300

    Add CharList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,3 @@
-
-
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=bef7e7189c9364cb60378baf085f4e57'
@@ -15,28 +13,19 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.result.map(this._transforCharacter);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transforCharacter(res.data.results[0]);
+        return this._transformCharacter(res.data.results[0]);
     }
 
-    _transforCharacter = (char) => {
-
-        // let desc = ""
-        // if (char.description.length === 0) {
-        //     desc += "This character has no info..."
-        // } if (char.description.length >= 225) {
-        //     console.log(char.description.length)
-        //     desc += char.description.slice(0, 225) + "..."
-        // } else desc += char.description
-
+    _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            // description: desc,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
